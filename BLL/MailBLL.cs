79f27d16823e2027264ace143c835605bff6fa5a
facingwaller <qinkaifang08@qq.com>
2016-09-13 09:05:59 +0800

    bll

---
+++ BLL/MailBLL.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace JiShi.BLL
+{
+    public class MailBll
+    {
+        public static void SendOneEmail(SmtpClient smtpClient, string from_email, string send_email, string subject, string body, string fileNamePath)
+        {
+            MailMessage mailMessage = new MailMessage();
+            // 指明邮件发送的地址，主题，内容等信息
+            // 发信人的地址为登录收发器的地址，这个收发器相当于我们平时Web版的邮箱或者是OutLook中配置的邮箱
+            mailMessage.From = new MailAddress(from_email);
+            mailMessage.To.Add(send_email);
+            mailMessage.Subject = subject;
+            mailMessage.SubjectEncoding = Encoding.Default;
+            mailMessage.Body = body;
+            mailMessage.BodyEncoding = Encoding.Default;
+            // 设置邮件正文不是Html格式的内容
+            mailMessage.IsBodyHtml = false;
+            // 设置邮件的优先级为普通优先级
+            mailMessage.Priority = MailPriority.Normal;
+            //mailMessage.ReplyTo = new MailAddress(tbxUserMail.Text);
+
+            //string extName = Path.GetExtension(fileNamePath).ToLower();
+
+            // 封装发送的附件
+            System.Net.Mail.Attachment attachment = null;
+
+            //if (extName == ".rar" || extName == ".zip")
+            //{
+            //    attachment = new System.Net.Mail.Attachment(fileNamePath, MediaTypeNames.Application.Zip);
+            //}
+            //else
+            //{
+            //    attachment = new System.Net.Mail.Attachment(fileNamePath, MediaTypeNames.Application.Octet);
+            //}
+
+
+            //mailMessage.Attachments.Add(attachment);
+
+
+            // 发送写好的邮件
+            try
+            {
+                // SmtpClient类用于将邮件发送到SMTP服务器
+                // 该类封装了SMTP协议的实现，
+                // 通过该类可以简化发送邮件的过程，只需要调用该类的Send方法就可以发送邮件到SMTP服务器了。
+                smtpClient.Send(mailMessage);
+                
+                //  LB_tips.Text = "send scuess!";
+                //    MessageBox.Show("邮件发送成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (SmtpException smtpError)
+            {
+                //MessageBox.Show("邮件发送失败：[" + smtpError.StatusCode + "]；["
+                //                + smtpError.Message + "];\r\n[" + smtpError.StackTrace + "]."
+                //    , "错误", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                mailMessage.Dispose();
+                //this.Cursor = Cursors.Default;
+            }
+        }
+
+
+
+
+         
+        private static bool FindEmailAddress(string fname, out string email)
+        {
+            email = "";
+            var res = Regex.Match(fname, "[(（《]*.com");
+            if (!res.Success) return false;
+
+            email = res.Value;
+            return true;
+        }
+    }
+}
---
+++ BLL/js_item.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+ 
+using JiShi.Model;
+namespace JiShi.BLL
+{
+	/// <summary>
+	/// js_item
+	/// </summary>
+	public partial class js_item
+	{
+		private readonly JiShi.DAL.js_item dal=new JiShi.DAL.js_item();
+		public js_item()
+		{}
+		#region  BasicMethod
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(string id)
+		{
+			return dal.Exists(id);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public bool Add(JiShi.Model.js_item model)
+		{
+			return dal.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(JiShi.Model.js_item model)
+		{
+			return dal.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(string id)
+		{
+			
+			return dal.Delete(id);
+		}
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool DeleteList(string idlist )
+		{
+			return dal.DeleteList(idlist );
+		}
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public JiShi.Model.js_item GetModel(string id)
+		{
+			
+			return dal.GetModel(id);
+		}
+
+        ///// <summary>
+        ///// 得到一个对象实体，从缓存中
+        ///// </summary>
+        //public JiShi.Model.js_item GetModelByCache(string id)
+        //{
+			
+        //    string CacheKey = "js_itemModel-" + id;
+        //    object objModel = Maticsoft.Common.DataCache.GetCache(CacheKey);
+        //    if (objModel == null)
+        //    {
+        //        try
+        //        {
+        //            objModel = dal.GetModel(id);
+        //            if (objModel != null)
+        //            {
+        //                int ModelCache = Maticsoft.Common.ConfigHelper.GetConfigInt("ModelCache");
+        //                Maticsoft.Common.DataCache.SetCache(CacheKey, objModel, DateTime.Now.AddMinutes(ModelCache), TimeSpan.Zero);
+        //            }
+        //        }
+        //        catch{}
+        //    }
+        //    return (JiShi.Model.js_item)objModel;
+        //}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return dal.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<JiShi.Model.js_item> GetModelList(string strWhere)
+		{
+			DataSet ds = dal.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<JiShi.Model.js_item> DataTableToList(DataTable dt)
+		{
+			List<JiShi.Model.js_item> modelList = new List<JiShi.Model.js_item>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				JiShi.Model.js_item model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = dal.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return dal.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return dal.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return dal.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
